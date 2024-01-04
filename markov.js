012888061f8cab23695b20e47c79ddb9d0e98872
Lauren DeLago <lcarinad@gmail.com>
2024-01-04 12:28:44 -0500

    files

---
+++ markov.js
@@ -0,0 +1,27 @@
+/** Textual markov chain generator */
+
+class MarkovMachine {
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+    let words = text.split(/[ \r\n]+/);
+    this.words = words.filter((c) => c !== "");
+    console.log(this.words);
+    this.makeChains();
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    // TODO
+  }
+
+  /** return random text from chains */
+
+  makeText(numWords = 100) {
+    // TODO
+  }
+}
