b35c2a374faf2b699e04b3c2f9bd4a3bb3fa83dc
Lauren DeLago <lcarinad@gmail.com>
2024-01-05 15:01:08 -0500

    makeText to read files

--- makeText.js
+++ makeText.js
@@ -1,2 +1,23 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require("fs");
+const MarkovMachine = require("./markov");
+const filePath = process.argv[2];
+
+function readFile(path) {
+  fs.readFile(filePath, "utf-8", function (err, data) {
+    if (err) {
+      console.error("This error was received:", err);
+      process.exit(1);
+    }
+    let text = data;
+    let mm = new MarkovMachine(text);
+    console.log(mm.makeText(20));
+  });
+}
+//read file at url
+readFile(filePath);
+
+function readUrl(url) {
+  const axios = require("axios");
+}
